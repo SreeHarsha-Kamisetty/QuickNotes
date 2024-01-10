bea1cd94675bbf1a2961ee6a38792c2ed38234d4
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-01-10 14:57:10 +0530

    Morgan middleware added for logs

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const {connection} = require("./db");
 const {UserRouter} = require("./routes/user.routes");
 const {NotesRouter} = require("./routes/notes.routes");
 const cors = require("cors");
-
+const morgan = require("morgan");
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use("/users",UserRouter);
 app.use("/notes",NotesRouter);
 
