a535583904fe8bee3800896e698be709dde6a7c8
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-01-11 08:25:59 +0530

    update functionality completed

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const {UserRouter} = require("./routes/user.routes");
 const {NotesRouter} = require("./routes/notes.routes");
 const cors = require("cors");
 const morgan = require("morgan");
-
+require('dotenv').config();
+const PORT = process.env.PORT || 8080
 
 const app = express();
 app.use(cors());
@@ -18,11 +19,11 @@ app.get("/",(req,res)=>{
     res.status(200).json({Message:"This is home page"});
 })
 
-app.listen(8080,async()=>{
+app.listen(PORT,async()=>{
     try {
         await connection;
         console.log("connected to DB");
-        console.log(`Server is running at http://localhost:8080`);
+        console.log(`Server is running at http://localhost:${PORT}`);
     } catch (error) {
         console.log(error);
     }
